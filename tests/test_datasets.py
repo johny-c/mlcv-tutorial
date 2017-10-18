acf837de93297a76d84e2c2b1b2c60fe67f868db
John Chiotellis <chiotell@in.tum.de>
2017-10-19 00:04:29 +0200

    attempt to fix testing with py.test

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,20 +1,12 @@
 from __future__ import absolute_import
-import numpy as np
-
 
 from mlcv.datasets import fetcher
 
 
-
 class TestFetcher:
-
-    def setUp(self):
-        np.random.seed(0)
-
-    def tearDown(self):
-        pass
 
     def test_load_iris(self):
         X, y = fetcher.load('iris')
-        self.assertTrue(len(X) == len(y))
-        self.assertTrue(X.shape == (150, 4))
+
+        assert(len(X) == len(y))
+        assert(X.shape == (150, 4))
